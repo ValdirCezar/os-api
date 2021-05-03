f28ce8d13d4493714ceb9bb45973ac4b013be1b3
Valdir Cezar <valdircezar312@gmail.com>
2021-05-03 20:41:20 -0300

    feat: free access to h2-console from browser

--- src/main/java/com/valdir/os/config/SecurityConfig.java
+++ src/main/java/com/valdir/os/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.valdir.os.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,15 +19,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/*" };
+	/*
+	 * Add injection of this Env to read a active profiles and free access to the
+	 * h2-console om browser
+	 */
+	@Autowired
+	private Environment environment;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
+		/*
+		 * This condition will verify if the active profile contais the test profile to
+		 * free access to h2-console om browser
+		 */
+		if (Arrays.asList(this.environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
 		// Enabling cors and disabling csrf
 		http.cors().and().csrf().disable();
 
-		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		// Ensuring that our application will not create a user session
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
