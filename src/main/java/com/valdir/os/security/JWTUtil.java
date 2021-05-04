fd109e2dbe229be3ca4e74a8631639e0066e5694
Valdir Cezar <valdircezar312@gmail.com>
2021-05-04 11:04:30 -0300

    feat: create class JWTUtil to generate token

---
+++ src/main/java/com/valdir/os/security/JWTUtil.java
@@ -0,0 +1,36 @@
+package com.valdir.os.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Value("${jwt.expiration}")
+	private String expiration;
+
+	/*
+	 * Method to generate a token
+	 */
+	public String generateToken(String cpf) {
+
+		/*
+		 * We will use the jwt now
+		 */
+		return Jwts.builder()
+				// Username
+				.setSubject(cpf)
+				// Time to expiration from token
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				// To say how I'm going to sign my token
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
+	}
+}
