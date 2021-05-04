356062171653d024475906be52ca5d6d2c35df7c
Valdir Cezar <valdircezar312@gmail.com>
2021-05-04 10:35:29 -0300

    feat: add UserSS class

---
+++ src/main/java/com/valdir/os/security/UserSS.java
@@ -0,0 +1,55 @@
+package com.valdir.os.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserSS implements UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String cpf;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return cpf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
